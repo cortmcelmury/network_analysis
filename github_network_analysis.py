178ca62630ef06cbf612c5dab0a2cc33ef47afbe
Cort McElmury <32118549+cortmcelmury@users.noreply.github.com>
2019-03-12 14:59:40 -0500

    Add files via upload

---
+++ github_network_analysis.py
@@ -0,0 +1,171 @@
+import pickle
+
+file = open('C:\\Users\cmcelmury\Documents\Python\Datasets\github_users.p', 'rb')
+G = pickle.load(file)
+file.close()
+
+
+import networkx as nx
+import matplotlib.pyplot as plt
+from nxviz import MatrixPlot, CircosPlot, ArcPlot
+
+# Characterize the network
+
+# Look at number of nodes and edges
+len(G.nodes())
+len(G.edges())
+
+# Plot the degree distribution of the GitHub collaboration network
+plt.hist(list(nx.degree_centrality(G).values()))
+plt.show()
+
+# Plot the betweenness distribution of the GitHub collaboration network
+plt.hist(list(nx.betweenness_centrality(G).values()))
+plt.show()
+
+################################################################################
+# Visualize the network
+################################################################################
+
+# MatrixPlot of largest subgraph
+# A subgraph is a graph in a network that is not connected by any nodes to another graph
+
+# Calculate the largest connected component subgraph: largest_ccs
+largest_ccs = sorted(nx.connected_component_subgraphs(G),
+                     key=lambda x: len(x))[-1]
+
+# Create the customized MatrixPlot object: h
+h = MatrixPlot(graph=largest_ccs, node_grouping='grouping')
+
+# Draw the MatrixPlot to the screen
+h.draw()
+plt.show()
+
+
+# ArcPlot of all nodes
+# Iterate over all the nodes in G, including the metadata
+for n, d in G.nodes(data=True):
+
+    # Calculate the degree of each node: G.node[n]['degree']
+    d['degree'] = nx.degree(G, n)
+
+# Create the ArcPlot object: a
+a = ArcPlot(graph=G, node_order='degree')
+
+# Draw the ArcPlot to the screen
+a.draw()
+plt.show()
+
+
+# CircosPlot of all nodes
+# Iterate over all the nodes, including the metadata
+for n, d in G.nodes(data=True):
+
+    # Calculate the degree of each node: G.node[n]['degree']
+    d['degree'] = nx.degree(G, n)
+
+# Create the CircosPlot object: c
+c = CircosPlot(graph=G, node_order='degree',
+               node_grouping='grouping', node_color='grouping')
+
+# Draw the CircosPlot object to the screen
+c.draw()
+plt.show()
+
+
+################################################################################
+# CLIQUES
+################################################################################
+
+# Calculate the maximal cliques in G: cliques
+cliques = nx.find_cliques(G)
+
+# Count and print the number of maximal cliques in G
+print(cliques)
+
+
+# Find the author(s) that are part of the largest maximal clique: largest_clique
+largest_clique = sorted(nx.find_cliques(G), key=lambda x: len(x))[-1]
+
+# Create the subgraph of the largest_clique: G_lc
+G_lc = G.subgraph(largest_clique)
+
+# Create the CircosPlot object: c
+c = CircosPlot(graph=G_lc)
+
+# Draw the CircosPlot to the screen
+c.draw()
+plt.show()
+
+
+################################################################################
+# FINAL TASKS
+#   Find important users
+#   Find largest communities of collaborators
+#   Build collaboration recommendation system based on open triangles
+################################################################################
+
+# Find important collaborators
+# Compute the degree centralities of G: deg_cent
+deg_cent = nx.degree_centrality(G)
+
+# Compute the maximum degree centrality: max_dc
+max_dc = max(deg_cent.values())
+
+# Find the user(s) that have collaborated the most: prolific_collaborators
+prolific_collaborators = [n for n, dc in deg_cent.items() if dc == max_dc]
+
+# Print the most prolific collaborator(s)
+print(prolific_collaborators)
+
+
+# Characterizing communities
+# Identify the largest maximal clique: largest_max_clique
+largest_max_clique = set(sorted(nx.find_cliques(G), key=lambda x: len(x))[-1])
+
+# Create a subgraph from the largest_max_clique: G_lmc
+G_lmc = G.subgraph(largest_max_clique).copy()
+
+# Go out 1 degree of separation
+for node in list(G_lmc.nodes()):
+    G_lmc.add_nodes_from(G.neighbors(node))
+    G_lmc.add_edges_from(
+        zip([node] * len(list(G.neighbors(node))), G.neighbors(node)))
+
+# Record each node's degree centrality score
+for n in G_lmc.nodes():
+    G_lmc.node[n]['degree centrality'] = nx.degree_centrality(G_lmc)[n]
+
+# Create the ArcPlot object: a
+a = ArcPlot(graph=G_lmc, node_order='degree centrality')
+
+# Draw the ArcPlot to the screen
+a.draw()
+plt.show()
+
+
+# Recommending co-editors who have yet to edit together (using open triangles)
+# Import necessary modules
+from itertools import combinations
+from collections import defaultdict
+
+# Initialize the defaultdict: recommended
+recommended = defaultdict(int)
+
+# Iterate over all the nodes in G
+for n, d in G.nodes(data=True):
+
+    # Iterate over all possible triangle relationship combinations
+    for n1, n2 in combinations(G.neighbors(n), 2):
+
+        # Check whether n1 and n2 do not have an edge
+        if not G.has_edge(n1, n2):
+
+            # Increment recommended
+            recommended[(n1, n2)] += 1
+
+# Identify the top 10 pairs of users
+all_counts = sorted(recommended.values())
+top10_pairs = [pair for pair, count in recommended.items() if count >
+               all_counts[-1]]
+print(top10_pairs)
